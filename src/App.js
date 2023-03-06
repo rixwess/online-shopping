201c587afea12e26fb90a2bab4f03233c84c1957
Yusufbek <yusufginger@gmail.com>
2023-03-06 19:47:15 +0500

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
+import { hover } from '@testing-library/user-event/dist/hover';
 import logo from './logo.svg';
-import './App.css';
+import Example from './Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Example/>
+  )
+  
 }
 
 export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+const Container = styled('div')`
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px 0;
+    
+`
+const Logo = styled ('a')`
+    color: black;
+    font-size:50px;
+    text-decoration:none;
+`
+
+const Ul = styled('ul')`
+    display: flex;
+    list-style: none;
+`
+const Button = styled('button')`
+border: none;
+padding: 15px 20px;
+border-radius: 8px;
+
+&:first-child{
+    color: black;
+}
+
+&:last-child{
+    color: #fff;
+    background-color: #3C9379;
+}
+`
+
+const Link = styled('a')`
+    font-family: 'Manrope';
+font-style: normal;
+font-weight: 500;
+font-size: 16px;
+text-decoration: none;
+margin: 0 20px;
+
+
+/* Grey */
+
+color: #535A56;
+`
+
+function Example() {
+
+
+  return (
+    <Container>
+        <Logo href=""> Selfie </Logo>
+        <Ul className="Nav">
+        <li><Link href="#1">Home</Link></li>
+        <li><Link href="#1">Products</Link></li>
+        <li><Link href="#1">Categorias</Link></li>
+        <li><Link href="#1">Brands</Link></li>
+        <li><Link href="#1">Pricing</Link></li>
+        </Ul>
+        <div>
+            <Button>Login</Button>
+            <Button>Register</Button>
+        </div>
+    </Container>
+  );
+}
+
+export default Example;
